7b31b0799f87f762d621f1f460e25539247f9dc8
Maitham Al-rubaye <maythem.altamar@gmail.com>
2023-07-25 01:26:17 +0200

    maven dependencies __ 2023

---
+++ mavendependency/src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import com.google.gson.Gson;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Gson gson = new Gson();
+        String json = gson.toJson("Hello World!");
+        System.out.println( json );
+    }
+}
